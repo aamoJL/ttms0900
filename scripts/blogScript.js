399aba016ee7b0488b8f9ac40040cf804e1d7a94
aamoJL <jonilaukka@hotmail.com>
2016-11-22 02:06:40 +0200

    siivous

--- scripts/blogScript.js
+++
@@ -1,6 +0,0 @@
-function newPost() {
-    if(document.getElementById("newPost").style.display == "block"){
-        document.getElementById("newPost").style.display = "none";
-    }
-    else {document.getElementById("newPost").style.display = "block";}
-}
