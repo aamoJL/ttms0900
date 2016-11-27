9c7fc75439f982a23ea8d2b22fc2d85ccceac355
aamoJL <jonilaukka@hotmail.com>
2016-11-28 01:16:36 +0200

    kuvien lisäys blogiin

--- blog.php
+++ blog.php
@@ -5,15 +5,22 @@
     <script src="scripts/adminScripts.js"></script> <!-- adminin scriptit -->
 </head>
 
+<?php
+include_once("includes/blogAdmin.php");
+?>
+
 <body>
 <?php include_once("includes/nav.html"); ?>
 <main class="blog">
         <button name="new" onclick="newPost()">Uusi</button>
         <section id=newBlogPost class="postList">
             <article id="newPost">
-				<input type=text id="postTitle" placeholder=Otsikko></input><br>
-                <textarea id="postText" rows="5" cols="50" placeholder="Teksti tähän..."></textarea><br>
-                <button id="add" onclick="addPost()">Add</button>
+				<form enctype="multipart/form-data" method=post action=<?php echo $_SERVER['PHP_SELF']; ?>>
+				<input name=otsikko type=text id="postTitle" placeholder=Otsikko></input><br>
+                <textarea name=viesti id="postText" rows="5" cols="50" placeholder="Teksti tähän..."></textarea><br>
+                <input name="kuva" type="file"><br>
+				<button name="add">Add</button>
+				</form>
             </article>
         </section>
         <section class="postList">
@@ -24,16 +31,22 @@
 					$posts = json_decode($data, true);
 					$posts = array_reverse($posts);
 					$index = count($posts) - 1;
+					$action = $_SERVER['PHP_SELF'];
 					
 					foreach($posts as $post){
-						echo "<article class='post'>
-							<p class='date'>".$post['paivays']."</p>
-							<h1 class='blogHeader'>".$post['otsikko']."</h1>
+						echo "<form action=".$action." method=post>
+							<article class='post'>
+							<p class='date'>".$post['paivays']."</p>";
+							if($post['kuva'] != ""){
+								echo "<img class=imgBlog src='blog-imgs/".$post['kuva']."' alt=''></img>";
+							}
+							echo "<h1 class='blogHeader'>".$post['otsikko']."</h1>
 							<p>".$post['viesti']."</p>
 							<hr>
 							<p class='footer'>".$post['lahettaja']."</p>
-							<button onclick='removePost(".$index.")'>X</buttton>
-							</article>";
+							<button name='remove' value='".$index."'>X</buttton>
+							</article>
+							</form>";
 							$index--;
 						}
 					}
