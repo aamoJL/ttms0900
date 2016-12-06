49409dd7e32439b6320c7a01f2ad54dbaf66a549
Jaakko Pöyhönen <jaakko.poyhonen@gmail.com>
2016-12-06 17:35:20 +0000

    Update changeVideo.php

--- Pleb/includes/changeVideo.php
+++ Pleb/includes/changeVideo.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 /*
 index.php
 Vaihtaa videoLog.db tietokantaan tilan ja videon.
@@ -6,63 +7,65 @@ Palauttaa seuraavan videon ID:n
 											!muokkaa error viestit!
 											!vaihda oikea tietokanta!
 */
-if(isset($_POST['next'])){
-	try{
-		$db = new SQLite3('/var/Databases/videoLog.db'); //tietokanta
-		
-		if($_POST['next'] == 'skip'){
-			//jos seuraava video on jonon ensimmäinen:
-			$videoDb = new SQLite3('/var/Databases/testdb.db'); //videotietokanta
-			$results = $videoDb->prepare("SELECT YTid FROM Video WHERE tila='jono' LIMIT 1;"); //hakee ensimmäisen jonossa olevan videon
-			$result = $results->execute(); //soitetuthaku
-			
-			$aika = date("Y-m-d H:i:s");
-			$newvideo = $result->fetchArray();
-			
-			if($newvideo[0] != ""){
-				$results = $db->prepare('UPDATE log SET date=:date, status=:status, videoid=:videoId WHERE id=1'); //tietokantahaun alustus
-				$results->bindValue(':date',$aika);
-				$results->bindValue(':status','playing');
-				$results->bindValue(':videoId',$newvideo[0]);
-				$result = $results->execute();
-				removeNew($newvideo[0]);
-				echo $newvideo[0];
-			}
-			else{
-				$results = $db->prepare('UPDATE log SET date=:date, status=:status, videoid=:videoId WHERE id=1'); //tietokantahaun alustus
-				$results->bindValue(':date',$aika);
-				$results->bindValue(':status','paused');
-				$results->bindValue(':videoId', "");
-				$result = $results->execute();
-				echo "";
-			}
-		}
-		else if($_POST['next'] == 'play'){
-			//jos seuraavan videon id annettu:
-			if(isset($_POST['videoid'])){
+if($_SESSION["user_name"] == "admin") {
+	if(isset($_POST['next'])){
+		try{
+			$db = new SQLite3('/var/Databases/videoLog.db'); //tietokanta
+
+			if($_POST['next'] == 'skip'){
+				//jos seuraava video on jonon ensimmäinen:
+				$videoDb = new SQLite3('/var/Databases/testdb.db'); //videotietokanta
+				$results = $videoDb->prepare("SELECT YTid FROM Video WHERE tila='jono' LIMIT 1;"); //hakee ensimmäisen jonossa olevan videon
+				$result = $results->execute(); //soitetuthaku
+
 				$aika = date("Y-m-d H:i:s");
-				$results = $db->prepare('UPDATE log SET date=:date, status=:status, videoid=:videoId WHERE id=1'); //tietokantahaun alustus
-				$results->bindValue(':date',$aika);
-				$results->bindValue(':status','playing');
-				$results->bindValue(':videoId', $_POST['videoid']);
-				$result = $results->execute();
-				removeNew($_POST['videoid']);
-				echo $_POST['videoid'];
+				$newvideo = $result->fetchArray();
+
+				if($newvideo[0] != ""){
+					$results = $db->prepare('UPDATE log SET date=:date, status=:status, videoid=:videoId WHERE id=1'); //tietokantahaun alustus
+					$results->bindValue(':date',$aika);
+					$results->bindValue(':status','playing');
+					$results->bindValue(':videoId',$newvideo[0]);
+					$result = $results->execute();
+					removeNew($newvideo[0]);
+					echo $newvideo[0];
+				}
+				else{
+					$results = $db->prepare('UPDATE log SET date=:date, status=:status, videoid=:videoId WHERE id=1'); //tietokantahaun alustus
+					$results->bindValue(':date',$aika);
+					$results->bindValue(':status','paused');
+					$results->bindValue(':videoId', "");
+					$result = $results->execute();
+					echo "";
+				}
 			}
+			else if($_POST['next'] == 'play'){
+				//jos seuraavan videon id annettu:
+				if(isset($_POST['videoid'])){
+					$aika = date("Y-m-d H:i:s");
+					$results = $db->prepare('UPDATE log SET date=:date, status=:status, videoid=:videoId WHERE id=1'); //tietokantahaun alustus
+					$results->bindValue(':date',$aika);
+					$results->bindValue(':status','playing');
+					$results->bindValue(':videoId', $_POST['videoid']);
+					$result = $results->execute();
+					removeNew($_POST['videoid']);
+					echo $_POST['videoid'];
+				}
+			}
+			/* else if($_POST['next'] == 'pause'){
+				//jos pausetetaan
+					$aika = date("Y-m-d H:i:s");
+					$results = $db->prepare('UPDATE log SET date=:date, status=:status, videoid=:videoId WHERE id=1'); //tietokantahaun alustus
+					$results->bindValue(':date',$aika);
+					$results->bindValue(':status','paused');
+					$results->bindValue(':videoId', '');
+					$result = $results->execute();
+					echo 'paused';
+			} */
 		}
-		/* else if($_POST['next'] == 'pause'){
-			//jos pausetetaan
-				$aika = date("Y-m-d H:i:s");
-				$results = $db->prepare('UPDATE log SET date=:date, status=:status, videoid=:videoId WHERE id=1'); //tietokantahaun alustus
-				$results->bindValue(':date',$aika);
-				$results->bindValue(':status','paused');
-				$results->bindValue(':videoId', '');
-				$result = $results->execute();
-				echo 'paused';
-		} */
-	}
-	catch(Exception $e){
-		echo "db error";
+		catch(Exception $e){
+			echo "db error";
+		}
 	}
 }
 else{
