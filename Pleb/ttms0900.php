9b6cd13ca3f1e8d694012a5cfb8464245d5150df
Jaakko Pöyhönen <jaakko.poyhonen@gmail.com>
2016-12-07 20:03:42 +0000

    Update ttms0900.php

--- Pleb/ttms0900.php
+++ Pleb/ttms0900.php
@@ -1,7 +1,6 @@
 <head>
     <meta charset="utf-8">
     <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script> <!-- jquery -->
     <link href="styles/style.css" type="text/css" rel=stylesheet>
 </head>
 
@@ -11,26 +10,87 @@
             <article class="dokumentti">
                 <h1 class="dokumenttiotsikko">Harjoitustyö: Pleblist</h1>
 				<h2>Kurssi: TTMS0900
-				<h3>Tekijät: Joni Laukka, Jaakko Pöyhönen - Päivä tähän
+				<h3>Tekijät: Joni Laukka, Jaakko Pöyhönen - 6.12.2016
                 <hr></hr>
 				<p>Lähdekoodit: Linkki tähän<br>
 				Zip-paketti: Linkki tähän
 				</p>
                 <hr>
-                <h2>Tehtävän kuvaus???????</h2>
-				<p>kuvausta kuvausta...</p>
+                <h2>Tehtävän kuvaus</h2>
+				<p>Linkki GitHub-suunnitelmaan?<br>
+					<ul class=dokumentti>
+					<li>Adminin johdolla toimiva automaattinen videosoittolista, johon voi lisätä kuka vain uusia videoita</li>
+					<li>kaikille sama soittolista, kaikilla soi adminin video.</li>
+					<li>Admin voi poistaa ja vaihtaa videoita</li>
+					<li>Sivustolla myös blogi, jossa posteja kehityksestä</li>
+					<li>Kirjautuminen admineille</li>
+					</ul>
+				</p>
 				<hr>
 				<h2>Toteutus</h3>
-                <p>Sovelluksen kokonaisrakenne mielellään graafisesti, luokkarakenne, funktioiden suhteet, sovelluksen suhteet tiedostoihin, tietokantoihin ...<br>
-				Funktiot ja luokat: ainakin tärkeimpien toiminta dokumentoitava, välitettävät parametrit<br>
-				Tietovarastot: tietokannat, tiedostot.</p>
+                <p>
+				Palvelin liittää kaikkiin sivuihin headerin, navin ja footerin includes/ -kansiosta.
+				<h4>footer.php toiminta</h4>
+					<ul class=dokumentti>
+						<li>Tulostaa palvelimen ajan.</li>
+					</ul>
+				<h4>nav.php toiminta</h4>
+					<ul class=dokumentti>
+						<li>Tulostaa kuvan sekä navigointilinkit sivulle.</li>
+					</ul>
+				<h3>blog.php</h3>
+				Palvelin liittää sivuun includes/blogAdmin.php:n adminin lomakkeita varten
+				<h4>blogAdmin.php toiminta</h4>
+				<ul class=dokumentti>
+					<li>Painettaessa "add"-nappia uuden merkinnän lomakkeesta, palvelin lisää databases/blog.json -tietokantaan uuden merkinnän tiedot (otsikko, päivä, viesti, lähettäjä, mahdollinen kuva).</li>
+						<ul class=dokumentti>
+						<li>palvelin tarkistaa onko lomakkeen kaikki kohdat täytetty, jos ei ole niin palvelin ilmoittaa "tyhjiä".</li>
+						<li>palvelin muuttaa otsikon ja viestin rakennetta php:n htmlspecialchars-funktiolla.</li>
+						<li>palvelin hyväksyy kuvan muodoksi vain jpg ja png.</li>
+						</ul>
+					<li>Painettaessa "X"-nappia merkinnästä, palvelin tekee databases/blog.json -tietokannasta uuden tietokannan, johon lisätään kaikki muut vanhat merkinnät, mutta ei poistettavaa merkintää.</li>
+							<ul class=dokumentti>
+							<li>jos poistettavalla merkinnällä on kuva, poistetaan kuvatiedosto blog-imgs/ -kansiosta.</li>
+							</ul>
+					<li>Palvelin tulostaa databases/blog.json -json tietokannasta blogimerkinnät, ensimmäinen merkintä ylimmäisenä.</li>
+						<ul class=dokumentti>
+						<li>"X"-nappiin vaihdetaan arvoksi blogimerkinnän indeksinumero.</li>
+						<li>jos merkinnällä on kuva, kuva lisätään nimen perusteella blog-imgs/ -kansiosta</li>
+						</ul>
+				</ul>
+				<h3>songlist.php</h3>
+				Sivu lataa includes/getQueue.php:stä ja includes/getPlayed.php:stä soittolistat.
+				<h4>getQueue.php toiminta</h4>
+				<ul class=dokumentti>
+					<li>Palvelin tulostaa taulun tietokannan tiedoista, joissa tila on 'jono'.</li>
+				</ul>
+				<h4>getPlayed.php toiminta</h4>
+				<ul class=dokumentti>
+					<li>Palvelin tulostaa taulun tietokannan tiedoista, joissa tila on 'soitettu'.</li>
+				</ul><br>
+				Painettaessa "X"-nappia videosta, sivu lähettää palvelimen includes/removeVideo.php:lle videon id:n.
+				<h4>removeVideo.php toiminta</h4>
+				<ul class=dokumentti>
+					<li>palvelin vaihtaa databases/testdb.db -tietokannasta annetun id:n tilaksi 'poistettu'.</li>
+				</ul>
+				</p>
 				<hr>
                 <h2>Ajan käyttö</h2>
-				<p>Selvitys työhön käytetystä ajasta ja tekijöiden roolista harjoitustyön tekemisessä sekä työn jakaantumisesta ryhmän sisällä.</p>
+				<p>
+				<ul class=dokumentti>
+					<li>15% UI</li>
+					<li>50% palvelin</li>
+					<li>35% client</li>
+				</ul>
+				</p>
 				<hr>
 				<h2>Itsearviot</h2>
 				<h3>Joni Laukka</h3>
-				<p>Jonin arvio</p>
+				<p>
+				Arvosana: 4<br>
+				Onnistumiset: Työ sisältää suunnitellut ominaisuudet ja toimii kuten pitääkin.<br>
+				Puutteet: -
+				</p>
 				
 				<h3>Jaakko Pöyhönen</h3>
 				<p>Jaakon arvio</p>
